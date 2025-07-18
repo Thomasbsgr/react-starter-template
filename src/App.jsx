76432eba27879114fb1dc573c2b90182c4895aad
Thomasbsgr <thomasbsgr@gmail.com>
2025-07-18 18:56:30 +0200

    add routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,13 @@
+// import { Routes, Route } from 'react-router-dom';
+
 function App() {
-  return <></>
+  return (
+    // <Routes>
+    //   <Route path="/" element={} />
+    //   <Route path="*" element={} />
+    // </Routes>
+    <h1>Ok !</h1>
+  )
 }
 
 export default App
