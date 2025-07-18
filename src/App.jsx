58b687d9c009756a9eb6da52e25e39d65f208a03
Thomasbsgr <thomasbsgr@gmail.com>
2025-07-18 20:22:50 +0200

    bug

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { Routes, Route } from 'react-router-dom';
-import Home from './pages/Home'
-import NotFound from './pages/NotFound'
+import Home from './pages/Home/Home'
+import NotFound from './pages/NotFound/NotFound'
 
 function App() {
   return (
