993c00920f1fd5169721050919a90fd349a80f56
Thomasbsgr <thomasbsgr@gmail.com>
2025-07-19 01:36:02 +0200

    prettier

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom'
 import Home from './pages/Home/Home'
 import NotFound from './pages/NotFound/NotFound'
 
